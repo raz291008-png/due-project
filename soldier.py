6a2e5e01fa32be66d0d4df8fb2743d1092ddc97a
raz291008 <raz291008@gmail.com>
2026-09-08 16:40:45 +0300

    t

--- soldier.py
+++ soldier.py
@@ -20,5 +20,15 @@ def find_legs(soldier):
 # def find_arms(soldier):
 #     lst = [soldier.get("x")]
 
-def move_soldier():
-    pass
+def move_soldier(dr,dc,soldier):
+    if soldier.get("row")+ dr > 49 or soldier.get("row")+ dr<0:
+        soldier["col"] = soldier.get("col") + dc
+
+    elif soldier.get("col")+dc >24 or soldier.get("col")+dc<0:
+        soldier["row"] = soldier.get("row") + dr
+
+    else:
+        soldier["row"] = soldier.get("row")+ dr
+        soldier["col"] = soldier.get("col")+dc
+
+
