1ce17a3d7cd275743519a9758a0669c38fc89ca0
raz291008 <raz291008@gmail.com>
2026-09-08 12:04:46 +0300

    t

--- gamefield.py
+++ gamefield.py
@@ -1,14 +1,37 @@
 import random
 import consts
 
-BOARD =[]
+game_field =[]
 def random_mine ():
-    pass
+    for row in range(20):
+        r = random.randint(0,consts.BOARD_ROWS-1)
+        c = random.randint(0,consts.BOARD_COLS-1)
+        place_mine(r,c)
+
+def place_mine(r,c):
+    if c == 0:
+        for i in range(3):
+            game_field[r][i]=consts.MINE_TILE
+
+    elif c == 49:
+        for i in range(47,50):
+            game_field[r][i] = consts.MINE_TILE
+
+    else:
+        for i in range(c-1,c+2):
+            game_field[r][i] = consts.MINE_TILE
 
 
-def create_():
-    global BOARD
+
+
+
+def create():
+    global game_field
     for r in range(consts.BOARD_ROWS):
-        BOARD.append([])
+        game_field.append([])
         for c in range(consts.BOARD_COLS):
-            BOARD[r][c].append(0)
+            game_field[r].append(0)
+
+
+def update():
+    pass
--- soldier.py
+++ soldier.py
@@ -2,10 +2,14 @@ import pygame
 import consts
 
 def create_soldier():
+
     pass
 
 def find_legs():
     pass
 
 def find_arms():
+    pass
+
+def move_soldier():
     pass
