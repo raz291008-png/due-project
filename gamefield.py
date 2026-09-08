0df1b741c963c69ef5f03726992db5e231a676b3
raz291008 <raz291008@gmail.com>
2026-09-08 11:09:51 +0300

    t

--- gamefield.py
+++ gamefield.py
@@ -1,3 +1,14 @@
+import random
+import consts
 
-def create():
+BOARD =[]
+def random_mine ():
     pass
+
+
+def create_():
+    global BOARD
+    for r in range(consts.BOARD_ROWS):
+        BOARD.append([])
+        for c in range(consts.BOARD_COLS):
+            BOARD[r][c].append(0)
