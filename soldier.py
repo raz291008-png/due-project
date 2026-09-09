66d0eaf7eb64169eafde4dd462cfccf718f4887b
raz291008 <raz291008@gmail.com>
2026-09-09 09:51:34 +0300

    t

--- soldier.py
+++ soldier.py
@@ -11,24 +11,37 @@ def create_soldier():
 
 
 def find_legs(soldier):
-    lst = [soldier.get("x")]
-    y = soldier.get("y")
-    y = consts.SOLDIER_BODY_ROWS * y
-    lst.append(y)
-    return lst
+    lst_legs = []
+    r = soldier.get("row")+consts.SOLDIER_BODY_ROWS
+    c = soldier.get("col")
+    for i in range(2):
+        lst_legs.append([r,c])
+        c = soldier.get("col") +1
+    return lst_legs
 
-# def find_arms(soldier):
-#     lst = [soldier.get("x")]
+def find_body(soldier):
+    lst_body = []
+    r = soldier.get("row")
+    c = soldier.get("col")
+    c2 = soldier.get("col")+1
+    for i in range (consts.SOLDIER_BODY_ROWS):
+        lst_body.append([r,c])
+        lst_body.append([r,c2])
+        r += 1
+    print(lst_body)
+
+
+
 
 def move_soldier(dr,dc,soldier):
-    if soldier.get("row")+ dr > 49 or soldier.get("row")+ dr<0:
+    if soldier.get("row")+ dr > consts.BOARD_COLS or soldier.get("row")+ dr<0:
         soldier["col"] = soldier.get("col") + dc
-
-    elif soldier.get("col")+dc >24 or soldier.get("col")+dc<0:
+        soldier["x"] = soldier.get("x") +(dc*20)
+    elif soldier.get("col")+dc > consts.BOARD_ROWS or soldier.get("col")+dc<0:
         soldier["row"] = soldier.get("row") + dr
-
+        soldier["y"] = soldier.get("y") + (dr*20)
     else:
         soldier["row"] = soldier.get("row")+ dr
-        soldier["col"] = soldier.get("col")+dc
-
-
+        soldier["col"] = soldier.get("col")+ dc
+        soldier["y"] = soldier.get("y") + (dr * 20)
+        soldier["x"] = soldier.get("x") + (dc * 20)
